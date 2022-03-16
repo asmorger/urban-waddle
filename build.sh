c365229e11f61e47631b5d758fe7c680d2e485a8
Andrew Morger <asmorger@live.com>
2022-03-15 19:26:54 -0500

    Renames keymap profile

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-qmk compile -kb ferris/sweep -km hawkbane-sweep
-cp ~/qmk_firmware/.build/ferris_sweep_hawkbane-sweep.hex /mnt/c/temp/sweep.hex
+qmk compile -kb ferris/sweep -km hawkbane
+cp ~/qmk_firmware/.build/ferris_sweep_hawkbane.hex /mnt/c/temp/sweep.hex
